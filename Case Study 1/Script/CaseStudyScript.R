bc5830d36ef365e72c74af062f766df188167b4d
firephotontorpedoes <firephotontorpedoes@gmail.com>
2017-10-16 15:46:00 -0500

    added code to markdown file

--- Case Study 1/Script/CaseStudyScript.R
+++ Case Study 1/Script/CaseStudyScript.R
@@ -3,20 +3,22 @@ library(data.table)
 library(dplyr)
 library(ggthemes)
 
+source('Script/makefile.R')
+
 #question 1
-q1 <- Brew %>% group_by(Brew$State) %>% summarize(n = n())
+q1 <- brew %>% group_by(brew$State) %>% summarize(n = n())
 head(q1)
 
 #Question 2
 head(suds, 6)
 tail(suds, 6)
 
-q3 <- sapply(q2, function(x) sum(length(which(is.na(q2)))))
+q3 <- sapply(suds, function(x) sum(length(which(is.na(suds)))))
 print(q3)
 
-q4 <- q2 %>% group_by(q2$State) %>% summarize(median_ABV = median(ABV), median_IBU = median(IBU))
+q4 <- suds %>% group_by(suds$State) %>% summarize(median_ABV = median(ABV), median_IBU = median(IBU))
 q4_df <- as.data.frame(q4)
-setnames(q4_df, "q2$State", "State")
+setnames(q4_df, "suds$State", "State")
 
 #bar chart for median_ABV
 q5a <- ggplot(data = q4, aes(x = State,y = median_ABV)) + 
@@ -34,16 +36,16 @@ print(q5a)
 print(q5b)
 
 #summary of ABV                                              
-q6 <- summary(q2$ABV)
+q6 <- summary(suds$ABV)
 print(q6)
 
 #scatter plot
-q7 <- ggplot(data = q2, aes(x = ABV,y = IBU)) + 
+q7 <- ggplot(data = suds, aes(x = ABV,y = IBU)) + 
   geom_point() +
   labs(x = 'ABV', y = 'IBU',title = 'International Bitterness Content vs. Alcoholic Content') +
   theme_economist()  
 print(q7)  
 
 #linear reegression model
-q7model <- lm(IBU ~ ABV, q2)
+q7model <- lm(IBU ~ ABV, suds)
 summary(q7model)
